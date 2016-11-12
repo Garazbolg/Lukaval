5e65a14e860032e678e0cdd9fa11f12f23d5c8b4
Garazbolg <legall.joseph@laposte.net>
2016-11-12 11:55:13 +0100

    Added KitchenFourniture Script This Script handle the behaviour of the kitchen fournitures

---
+++ Assets/Scripts/Kitchen/KitchenFourniture.cs
@@ -0,0 +1,63 @@
+#using System;
+#using UnityEngine;
+
+public class KitchenFourniture : MonoBehaviour
+{
+    ///Time CLosed In Seconds
+    public float closedTime = 3f;
+    
+    ///Time the fourniture stays open in Seconds
+    public float openTime = 2f;
+    
+    ///Time the fourniture takes to open in seconds
+    public float openingTime = 0.5f;
+    
+    ///Time the fourniture takes to close in seconds
+    public float closingTime = 0.5f;
+    
+    #region TargetTransform
+    
+        public Vector3 targetPosition;
+        public Quaternion targetRotation;
+        // TargetScale ?
+        
+        public Vector3 startPosition;
+        public Quaternion startRotation;
+    
+    #endregion
+    
+    private currentTime = 0f;
+    
+    void Start()
+    {
+        currentTime = Random.Range(0.0f,closedTime);
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+    }
+    
+    void Update(){
+        currentTime += Time.deltaTime;
+        currentTime -= (int)(currentTime/(closedTime + openingTime + openTime + closingTime))*(closedTime + openingTime + openTime + closingTime);
+        if(currentTime<closedTime){
+            transform.position = startPosition;
+            transform.rotation = startRotation;
+        }
+        else if(currentTime < closedTime + openingTime){
+            transform.position = Vector3.Lerp(startPosition,targetPosition,(currentTime-closedTime)/openingTime);
+            transform.rotation = Quaternion.Slerp(startRotation,targetRotation,(currentTime-closedTime)/openingTime);
+        }
+        else if(currentTime < closedTime + openingTime + openTime){
+            transform.position = targetPosition;
+            transform.rotation = targetRotation;
+        }
+        else{
+            transform.position = Vector3.Lerp(targetPosition,startPosition,(currentTime-closedTime-openingTime-openTime)/closingTime);
+            transform.rotation = Quaternion.Slerp(targetRotation,startRotation,(currentTime-closedTime-openingTime-openTime)/closingTime);
+        }
+    }
+    
+    public void Reset(){
+     transform.position = startPosition;
+     transform.rotation = startRotation;
+    }
+}

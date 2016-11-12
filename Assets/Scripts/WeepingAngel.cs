f7d7821bffe5a914bf9fc14e4b2ba78a08bd6384
Garazbolg <legall.joseph@laposte.net>
2016-11-12 18:03:44 +0100

    Added Weeping Angels Scripts
    Also added .blend to .gitignore

---
+++ Assets/Scripts/WeepingAngel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeepingAngel : MonoBehaviour {
+
+    private Transform player;
+    private float fov;
+
+    public float speed = 3f;
+
+	// Use this for initialization
+	void Start () {
+        fov = Camera.main.fieldOfView;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Angle(player.forward, transform.position - player.position) > fov) // En dehors du champ de vision
+        {
+            transform.position += (player.position - transform.position).normalized * Time.deltaTime * speed;
+        }
+	}
+}

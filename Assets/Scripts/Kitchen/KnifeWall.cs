6cec5377169edb28c981432389863236a7bd60b2
Garazbolg <legall.joseph@laposte.net>
2016-11-13 18:09:49 +0100

    Knife Animation for Kitchen

--- Assets/Scripts/Kitchen/KnifeWall.cs
+++ Assets/Scripts/Kitchen/KnifeWall.cs
@@ -6,17 +6,24 @@ public class KnifeWall : MonoBehaviour{
 
 	public float speed = 3f;
 
+	private Transform[] knifes;
+	public float knifeSpeed = 90; //in degrees per seconds
+
     void Start()
     {
         foreach(KitchenFourniture kf in FindObjectsOfType<KitchenFourniture>())
         {
             kf.Reset();
         }
-
+		knifes = GetComponentsInChildren<Transform>();
     }
 
 	void Update(){
 		transform.position += transform.forward.normalized * speed * Time.deltaTime;
+
+		for(int i = 1 ; i < knifes.Length;i++){
+			knifes[i].Rotate(Vector3.right,knifeSpeed * Time.deltaTime,Space.Self);
+		}
 	}
 
 	void OnTriggerEnter(Collider other){

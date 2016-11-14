5fddc3dcf6ef2b98633246e4169a09a4c52a47b3
Remi <rfpierot91@gmail.com>
2016-11-14 05:24:36 +0100

    Complete game scene with map switches.

---
+++ Assets/Scripts/SwapMap.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwapMap : MonoBehaviour {
+    public GameObject toKillScene;
+    public GameObject nextScene;
+    public GameObject lightForAttic;
+
+    void OnTriggerEnter(Collider other)
+    {
+        toKillScene.SetActive(false);
+        nextScene.SetActive(true);
+        lightForAttic.SetActive(nextScene.name.Equals("Grenier"));
+    }
+}
